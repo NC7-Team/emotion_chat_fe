48874cd8fad20eb450321dd3ad49483e85f82cc0
same-woo <pardongwoo@naver.com>
2023-10-16 20:21:40 +0900

    "feat : 카카오 로그인 구현"

--- src/user/login/Login.js
+++ src/user/login/Login.js
@@ -1,80 +1,54 @@
 import React, { Component } from "react";
 import "./Login.css";
 import {
-  GOOGLE_AUTH_URL,
-  FACEBOOK_AUTH_URL,
-  GITHUB_AUTH_URL,
-  ACCESS_TOKEN,
+    FACEBOOK_AUTH_URL,
+    GOOGLE_AUTH_URL,
+    KAKAO_AUTH_URL,
+    // NAVER_AUTH_URL, /* 다른 상수들도 임포트하세요 */
 } from "../../constants";
-import { Link } from "react-router-dom";
 import fbLogo from "../../img/fb-logo.png";
 import googleLogo from "../../img/google-logo.png";
 import kakaoLogo from "../../img/kakao-logo.png";
-import naveLogo from "../../img/naver-logo.png";
-import Alert from "react-s-alert";
+import naverLogo from "../../img/naver-logo.png";
 
 class Login extends Component {
-  componentDidMount() {
-    // this.props.location을 확인하고 state를 읽기 전에 state가 존재하는지 확인
-    if (this.props.location && this.props.location.state) {
-      const { error } = this.props.location.state;
-      if (error) {
-        setTimeout(() => {
-          Alert.error(error, {
-            timeout: 5000,
-          });
-          this.props.history.replace({
-            pathname: this.props.location.pathname,
-            state: {},
-          });
-        }, 100);
-      }
-    }
+    // 생략...
 
-    if (this.props.authenticated) {
-      this.props.history.push("/");
+    render() {
+        return (
+            <div className="login-container">
+                <div className="login-content">
+                    <h1 className="login-title">Login to Mood Canvas</h1>
+                    <SocialLogin />
+                </div>
+            </div>
+        );
     }
-  }
-
-  render() {
-    return (
-        <div className="login-container">
-          <div className="login-content">
-            <h1 className="login-title">Login to Mood Canvas</h1>
-            <SocialLogin />
-          </div>
-        </div>
-    );
-  }
 }
 
 class SocialLogin extends Component {
-  render() {
-    return (
-        <div className="social-login">
+    render() {
+        return (
+            <div className="social-login">
+                <a className="btn btn-block social-btn github" href={GOOGLE_AUTH_URL}>
+                    <img src={googleLogo} alt="Google" /> Log in with Google
+                </a>
 
-          <a className="btn btn-block social-btn github" href={GOOGLE_AUTH_URL}>
-            <img src={googleLogo} alt="Google" /> Log in with Google
-          </a>
+                <a className="btn btn-block social-btn github" href={KAKAO_AUTH_URL}>
+                    <img src={kakaoLogo} alt="Kakao" /> Log in with Kakao
+                </a>
 
-          <a className="btn btn-block social-btn github" href={GOOGLE_AUTH_URL}>
-            <img src={kakaoLogo} alt="Kakao" /> Log in with Kakao
-          </a>
+                <a className="btn btn-block social-btn github" href={KAKAO_AUTH_URL}>
+                    <img src={naverLogo} alt="Naver" /> Log in with Naver
+                </a>
 
-          <a className="btn btn-block social-btn github" href={GOOGLE_AUTH_URL}>
-            <img src={naveLogo} alt="Kakao" /> Log in with Naver
-          </a>
-
-          <a
-              className="btn btn-block social-btn facebook"
-              href={FACEBOOK_AUTH_URL}
-          >
-            <img src={fbLogo} alt="Facebook" /> Log in with Facebook
-          </a>
-
-        </div>
-    );
-  }
+                <a className="btn btn-block social-btn facebook" href={FACEBOOK_AUTH_URL}>
+                    <img src={fbLogo} alt="Facebook" /> Log in with Facebook
+                </a>
+            </div>
+        );
+    }
 }
+
 
 export default Login;
