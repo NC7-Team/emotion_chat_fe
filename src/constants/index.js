7e09a5032c8d027519255743c8abce0bee94a475
same-woo <pardongwoo@naver.com>
2023-10-17 14:57:55 +0900

    "fix : 도메인 로그인 경로 수정"

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,9 @@
-// export const API_BASE_URL = 'http://localhost:8080';
-export const API_BASE_URL = 'http://moodcanvas.site';
-export const ACCESS_TOKEN = 'accessToken';
-
+// export const API_BASE_URL = 'http://moodcanvas.site';
 // export const OAUTH2_REDIRECT_URI = 'http://localhost:3000/oauth2/redirect'
+export const ACCESS_TOKEN = 'accessToken';
+export const API_BASE_URL = 'http://localhost:80';
 export const OAUTH2_REDIRECT_URI = 'http://moodcanvas.site/oauth2/redirect'
+
 
 export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
 export const KAKAO_AUTH_URL = API_BASE_URL + '/oauth2/authorize/kakao?redirect_uri=' + OAUTH2_REDIRECT_URI;
