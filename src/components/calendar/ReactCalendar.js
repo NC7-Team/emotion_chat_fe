a57490862c2c7c2423ecafd94e179a25ae4a255f
data04190 <data04190@naver.com>
2023-10-24 02:03:08 +0900

    feat : 사용자별 감정 달력 생성

--- src/components/calendar/ReactCalendar.js
+++ src/components/calendar/ReactCalendar.js
@@ -8,7 +8,7 @@ import moment from "moment";
 
 moment.locale("en");
 
-function App() {
+function App(props) {
   const [emotions, setEmotions] = useState({});
 
   const emotionIcons = {
@@ -19,7 +19,7 @@ function App() {
 
   useEffect(() => {
     axios
-      .get(`${process.env.REACT_APP_API_ENDPOINT}/api/chatlogs/emotions/1`)
+      .get(`${process.env.REACT_APP_API_ENDPOINT}/api/chatlogs/emotions/${props.currentUser.id}`)
       .then((response) => {
         setEmotions(response.data);
       })
