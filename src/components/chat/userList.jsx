d0f3b5d93f430c8bd719e45f117940c6b4b146aa
same-woo <pardongwoo@naver.com>
2023-10-23 17:16:18 +0900

    "fix : http 경로 수정"

--- src/components/chat/userList.jsx
+++ src/components/chat/userList.jsx
@@ -8,7 +8,7 @@ const UserList = ({ roomId }) => {
 
   const fetchUserList = async () => {
     try {
-      const response = await fetch(`http://localhost:8080/chatUsers/${roomId}`);
+      const response = await fetch(`https://www.moodcanvas.site/chatUsers/${roomId}`);
       if (!response.ok) {
         throw new Error('네트워크 응답이 실패했습니다');
       }
