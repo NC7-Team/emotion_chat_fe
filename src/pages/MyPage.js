652cbe1240975e002af8731a2ef787a75e68cb8f
data04190 <data04190@naver.com>
2023-10-24 01:49:56 +0900

    feat : currentUser 확인

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -4,20 +4,21 @@ import Diary from "../components/diary/Diary";
 import Recommend from "../components/recommend/Recommend";
 
 export default function MyPage(props) {
-    return (
-        <div className="my-page">
-            <div className="diary-area">
-                <Diary />
-            </div>
-            <div>
-                <ReactCalendar />
-            </div>
-            <div className="recommend-area">
-                <Recommend />
-            </div>
-            <div className="user-name">
-                <span>{props.currentUser.name}</span>
-            </div>
-        </div>
-    );
+  console.log(props.currentUser);
+  return (
+    <div className="my-page">
+      <div className="diary-area">
+        <Diary />
+      </div>
+      <div>
+        <ReactCalendar />
+      </div>
+      <div className="recommend-area">
+        <Recommend />
+      </div>
+      <div className="user-name">
+        <span>{props.currentUser.name}</span>
+      </div>
+    </div>
+  );
 }
