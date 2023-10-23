c9614b9d54a22639c38a3cf6433df805f8ddfe84
ShaniceKim <qxzo1@naver.com>
2023-10-23 14:26:17 +0900

    fix : userList 부분 수정

--- src/components/chat/RoomList.jsx
+++ src/components/chat/RoomList.jsx
@@ -17,18 +17,18 @@ export default function RoomList() {
   }, [location])
 
 
-  const getRoomName = (roomIndex) => {
-    switch (roomIndex) {
-      case 1:
-        return "화남";
-      case 2:
-        return "슬픔";
-      case 3:
-        return "기쁨";
-      default:
-        return "알 수 없음";
-    }
-  };
+  // const getRoomName = (roomIndex) => {
+  //   switch (roomIndex) {
+  //     case 1:
+  //       return "화남";
+  //     case 2:
+  //       return "슬픔";
+  //     case 3:
+  //       return "기쁨";
+  //     default:
+  //       return "알 수 없음";
+  //   }
+  // };
 
   const { connected, currentRoomIndex, roomIndices } = messageStore;
 
@@ -52,7 +52,7 @@ export default function RoomList() {
         <ul className="room-list">
           {roomIndices.map((roomIndex) => (
             <li key={roomIndex} className="room-list-item">
-              <button
+              {/* <button
                 type="button"
                 disabled={roomIndex === currentRoomIndex}
                 onClick={() =>
@@ -63,17 +63,10 @@ export default function RoomList() {
                 }
               >
                 {getRoomName(roomIndex)} 채팅방
-              </button>
+              </button> */}
             </li>
           ))}
         </ul>
-        <button
-          type="button"
-          disabled={!connected}
-          onClick={() => handleClickQuitRoom()}
-        >
-          연결 종료
-        </button>
         <UserList roomId={roomId} />
       </div>
     </div>
