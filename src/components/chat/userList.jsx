d2bdd9795df448a2cb6d4362bebb7c8ca5e014ee
data04190 <data04190@naver.com>
2023-10-25 01:39:09 +0900

    fix : API 오류 수정

--- src/components/chat/userList.jsx
+++ src/components/chat/userList.jsx
@@ -8,7 +8,7 @@ const UserList = ({ roomId }) => {
 
   const fetchUserList = async () => {
     try {
-      const response = await fetch(`https://www.moodcanvas.site//chatUsers/${roomId}`);
+      const response = await fetch(`https://www.moodcanvas.site/chatUsers/${roomId}`);
       if (!response.ok) {
         throw new Error('네트워크 응답이 실패했습니다');
       }
@@ -36,7 +36,7 @@ const UserList = ({ roomId }) => {
   }, [roomId]);
 
   return (
-    
+
     <div className="user-list-container">
       {loading ? (
         <div>로딩 중...</div>
